79f54c20d45b527188c08e11d80b3ff8334b21a5
Aritra <aritrapersonal.20@gmail.com>
2024-07-26 19:52:10 +0530

    Update WordDocument.java

--- Week 1/Week 1 Design Patterns and Principles/FactoryMethodPatternExample/src/WordDocument.java
+++ Week 1/Week 1 Design Patterns and Principles/FactoryMethodPatternExample/src/WordDocument.java
@@ -1,11 +1,3 @@
-public class WordDocument implements Document{
-    public void open(){
-        System.out.println("Opening Word document!");
-    }
-    public void save(){
-        System.out.println("Saving Word Document!");
-    }
-    public void close(){
-        System.out.println("Closing Word document!");
-    }
+public interface WordDocument extends Document{
+  
 }
