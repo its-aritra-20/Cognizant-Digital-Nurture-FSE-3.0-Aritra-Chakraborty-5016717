d9b0c7db02bc186872107afd5df101bdc7a66f77
Aritra <aritrapersonal.20@gmail.com>
2024-07-27 20:06:40 +0530

    Delete Test.java

--- Week 1/Week 1 Design Patterns and Principles/SingletonPatternExample/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Logger logger1=Logger.getInstance();
-        Logger logger2=Logger.getInstance();
-        System.out.println(logger1==logger2);
-        logger1.log("Hello this is logger1!");
-        logger2.log("Hello this is logger2!");
-    }
-}
